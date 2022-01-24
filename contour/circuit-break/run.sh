0be43910f6e4126e41968564d6505a072009f0c3
lewgun <lewgun@163.com>
2022-01-24 17:50:24 +0800

    refactor & shell

---
+++ contour/circuit-break/run.sh
@@ -0,0 +1,1 @@
+ hey -n 200 -c 10 -m GET http://local.projectcontour.io/
---
+++ contour/ratelimit/global/contour.sh
@@ -0,0 +1,1 @@
+contour serve --insecure --xds-address=0.0.0.0 --envoy-service-http-port=80 --envoy-service-https-port=443 --debug  --config-path=./04-contour-config-dev.yaml
